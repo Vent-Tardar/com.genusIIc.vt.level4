dc4da42214eea02f5cf1de4785f4a1f54906d5d0
Vent-Tardar <varvara270802@bk.ru>
2021-04-28 10:20:24 +0300

    Getting started with Git

---
+++ src/main/java/com/genusIIc/vt/Main.java
@@ -0,0 +1,21 @@
+package com.genusIIc.vt;
+
+import org.apache.logging.log4j.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) throws Exception {
+        if ((args.length > 2) || (args.length < 2)){
+            logger.error("Parameters entered incorrectly");
+        } else {
+            ComparisonDoc cd = new ComparisonDoc();
+            List<String> lst = new ArrayList();
+            lst = cd.compare(args[0], args[1]);
+            for (String s : lst) {
+                System.out.println(s);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/genusIIc/vt/ComparisonDocTest.java
@@ -0,0 +1,27 @@
+package com.genusIIc.vt;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ComparisonDocTest {
+   @Test
+    void compareTest(){
+      ClassLoader classLoader = getClass().getClassLoader();
+
+      File test_file_1 = new File(classLoader.getResource("test1.txt").getFile());
+      String absolutePath_1 = test_file_1.getAbsolutePath();
+
+      File test_file_2 = new File(classLoader.getResource("test2.txt").getFile());
+      String absolutePath_2 = test_file_2.getAbsolutePath();
+
+      ComparisonDoc cd = new ComparisonDoc();
+      List lst = new ArrayList();
+      lst = cd.compare(absolutePath_1, absolutePath_2);
+      assertEquals(8, lst.size());
+   }
+}
