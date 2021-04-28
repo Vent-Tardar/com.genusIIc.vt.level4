377657e0f313dc85e2a78f864f881afd9decb577
Vent-Tardar <varvara270802@bk.ru>
2021-04-28 12:31:48 +0300

    Modifying tests and adding new test files

--- src/test/java/com/genusIIc/vt/ComparisonDocTest.java
+++ src/test/java/com/genusIIc/vt/ComparisonDocTest.java
@@ -1,10 +1,13 @@
 package com.genusIIc.vt;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ComparisonDocTest {
@@ -22,7 +25,7 @@ public class ComparisonDocTest {
     }
 
     @Test
-    public void filecomparisonTest() throws Exception {
+    public void filecomparisonTest() {
         File test_file_1 = new File(classLoader.getResource("test1.txt").getFile());
         String absolutePath_1 = test_file_1.getAbsolutePath();
         File test_file_2 = new File(classLoader.getResource("test1.txt").getFile());
@@ -37,7 +40,21 @@ public class ComparisonDocTest {
     }
 
    @Test
-    void compareTest(){
+   @Timeout(4)
+   public void compareSmallTest(){
+       File test_file_1 = new File(classLoader.getResource("smallTest_1.txt").getFile());
+       String absolutePath_1 = test_file_1.getAbsolutePath();
+       File test_file_2 = new File(classLoader.getResource("smallTest_2.txt").getFile());
+       String absolutePath_2 = test_file_2.getAbsolutePath();
+       ComparisonDoc cd = new ComparisonDoc();
+       List lst = new ArrayList();
+       lst = cd.compare(absolutePath_1, absolutePath_2);
+       assertEquals(2, lst.size());
+   }
+
+   @Test
+   @Timeout(6)
+   public void compareMediumTest(){
        File test_file_1 = new File(classLoader.getResource("test1.txt").getFile());
        String absolutePath_1 = test_file_1.getAbsolutePath();
        File test_file_2 = new File(classLoader.getResource("test2.txt").getFile());
@@ -47,4 +64,18 @@ public class ComparisonDocTest {
        lst = cd.compare(absolutePath_1, absolutePath_2);
        assertEquals(8, lst.size());
    }
+
+   @Test
+   @Timeout(6)
+   public void compareBigTest(){
+       File test_file_1 = new File(classLoader.getResource("bigTest_1.txt").getFile());
+       String absolutePath_1 = test_file_1.getAbsolutePath();
+       File test_file_2 = new File(classLoader.getResource("bigTest_2.txt").getFile());
+       String absolutePath_2 = test_file_2.getAbsolutePath();
+       ComparisonDoc cd = new ComparisonDoc();
+       List lst = new ArrayList();
+       lst = cd.compare(absolutePath_1, absolutePath_2);
+       assertEquals(6, lst.size());
+   }
+
 }
